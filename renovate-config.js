f513f1a00e0d256231a427b46fdfff3dc899170c
Johan Janssen <johanjanssen2001@gmail.com>
2022-04-08 21:45:41 +0200

    Provided Renovate platform

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   branchPrefix: 'test-renovate/',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  platform: 'github',
   repositories: [
     'johanjanssen/JavaHiddenGems'
   ]
