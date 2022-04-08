673ef931dd8ab057165669e50aca353bf1a2d18d
Johan Janssen <johanjanssen2001@gmail.com>
2022-04-08 21:36:46 +0200

    Renovate config.

--- renovate-config.js
+++ renovate-config.js
@@ -1,24 +1,4 @@
 module.exports = {
   branchPrefix: 'test-renovate/',
-  dryRun: true,
-  username: 'renovate-release',
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
-  platform: 'github',
-  includeForks: true,
-  packageRules: [
-    {
-      description: 'lockFileMaintenance',
-      matchUpdateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
-      ],
-      dependencyDashboardApproval: false,
-      stabilityDays: 0,
-    },
-  ],
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>'
 };
