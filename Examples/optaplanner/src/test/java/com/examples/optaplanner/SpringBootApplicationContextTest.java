34293f02dc8fe49a781be6ea55740fa0b96144bb
Johan Janssen <johanjanssen2001@gmail.com>
2023-03-03 20:56:46 +0100

    Added test

---
+++ Examples/optaplanner/src/test/java/com/examples/optaplanner/SpringBootApplicationContextTest.java
@@ -0,0 +1,13 @@
+package com.examples.optaplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationContextTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
