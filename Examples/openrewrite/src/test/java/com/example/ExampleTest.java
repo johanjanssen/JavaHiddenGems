41ca3567bc38434f8939cd874cd7655c0de7bef6
Tim te Beek <tim@moderne.io>
2023-03-12 10:47:44 +0100

    Showcase TemporaryFolder Rule and expected exception replacements

--- Examples/openrewrite/src/test/java/com/example/ExampleTest.java
+++ Examples/openrewrite/src/test/java/com/example/ExampleTest.java
@@ -1,13 +1,37 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import static org.junit.Assert.assertEquals;
+import java.io.File;
+import java.io.IOException;
 
 public class ExampleTest {
 
     @Test
     public void simpleTest() {
-        assertEquals(42, 42);
+        Assert.assertEquals(42, 42);
+    }
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void temporaryFileTest() throws IOException {
+        File testFile = folder.newFile("test.txt");
+        Assert.assertTrue(testFile.exists());
+    }
+
+    @Test(expected = IOException.class)
+    public void expectedExceptionTest() throws IOException {
+        maybeThrowException(true);
+    }
+
+    private void maybeThrowException(boolean maybe) throws IOException {
+        if (maybe) {
+            throw new IOException();
+        }
     }
 }
