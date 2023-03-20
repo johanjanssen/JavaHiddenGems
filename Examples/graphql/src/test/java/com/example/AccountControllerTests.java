4a8090acb3ebd2e19eb2d7696ca5403bbfba8c96
sohamda <iam.soham@gmail.com>
2023-03-20 15:26:47 +0100

    added dgs code generation plugin to remove string matching from tests

--- Examples/graphql/src/test/java/com/example/AccountControllerTests.java
+++ Examples/graphql/src/test/java/com/example/AccountControllerTests.java
@@ -1,9 +1,16 @@
 package com.example;
 
+import com.example.graphql.model.client.AccountByIdGraphQLQuery;
+import com.example.graphql.model.client.AccountByIdProjectionRoot;
+import com.example.graphql.model.types.Account;
+import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @GraphQlTest(AccountController.class)
 public class AccountControllerTests {
@@ -13,34 +20,34 @@ public class AccountControllerTests {
 
     @Test
     void shouldGetFirstAccount() {
-        this.graphQlTester
-                .documentName("account") // see the corresponding file under src/test/resources
-                .variable("id", "account-1")
-                .execute()
-                .path("accountById")
-                .matchesJson("""
-                    {
-                        "id": "account-1",
-                        "number": 42
-                    }
-                """);
+        // arrange
+        GraphQLQueryRequest graphQLQueryRequest = new GraphQLQueryRequest(
+                AccountByIdGraphQLQuery.newRequest().id("account-1").build()
+                , new AccountByIdProjectionRoot().id().number() );
+        // act
+        GraphQlTester.Entity<Account, ?> accountById = this.graphQlTester
+                .document(graphQLQueryRequest.serialize())
+                .execute().path("accountById").entity(Account.class);
+        // assert
+        assertEquals("account-1", accountById.get().getId());
+        assertEquals(42, accountById.get().getNumber());
+        assertNull(accountById.get().getUser());
+
     }
     @Test
     void shouldGetFirstAccountWithUser() {
-        this.graphQlTester
-				.documentName("accountWithUser")// see the corresponding file under src/test/resources
-				.variable("id", "account-1")
-                .execute()
-                .path("accountById")
-                .matchesJson("""
-                    {
-                        "id": "account-1",
-                        "number": 42,
-                        "user": {
-                          "firstName": "James",
-                          "lastName": "Gosling"
-                        }
-                    }
-                """);
+        // arrange
+        GraphQLQueryRequest graphQLQueryRequest = new GraphQLQueryRequest(
+                AccountByIdGraphQLQuery.newRequest().id("account-1").build()
+                , new AccountByIdProjectionRoot().id().number().user().firstName().lastName() );
+        // act
+        GraphQlTester.Entity<Account, ?> accountById = this.graphQlTester
+                .document(graphQLQueryRequest.serialize())
+                .execute().path("accountById").entity(Account.class);
+        // assert
+        assertEquals("account-1", accountById.get().getId());
+        assertEquals(42, accountById.get().getNumber());
+        assertEquals("James", accountById.get().getUser().getFirstName());
+        assertEquals("Gosling", accountById.get().getUser().getLastName());
     }
 }
