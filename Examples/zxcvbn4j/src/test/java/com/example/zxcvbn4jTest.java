aa4fff731bdc73582e7c56cf1d99224c2109ec5a
Johan Janssen <johanjanssen2001@gmail.com>
2024-12-10 09:19:45 +0100

    Added some examples for zxcvbn4j

--- Examples/zxcvbn4j/src/test/java/com/example/zxcvbn4jTest.java
+++ Examples/zxcvbn4j/src/test/java/com/example/zxcvbn4jTest.java
@@ -20,9 +20,21 @@ public class zxcvbn4jTest {
         assertEquals(1, strength.getScore());
         assertEquals(720000.0, strength.getCrackTimeSeconds().getOnlineThrottling100perHour());
 
-        strength = zxcvbn.measure("e!f.M&@6ycvN3E[s)nPp=7\n");
+        String password = "e!f.M&@6ycvN3E[s)nPp=7";
+        assertEquals(22, password.length());
+        strength = zxcvbn.measure(password);
         assertEquals(4, strength.getScore());
-        assertEquals(3.6E24, strength.getCrackTimeSeconds().getOnlineThrottling100perHour());
+        assertEquals(3.6E23, strength.getCrackTimeSeconds().getOnlineThrottling100perHour());
+
+        password = "this is just a very lo";
+        assertEquals(22, password.length());
+        strength = zxcvbn.measure(password);
+        assertEquals(4, strength.getScore());
+        assertEquals(8.6486436E19, strength.getCrackTimeSeconds().getOnlineThrottling100perHour());
+
+        strength = zxcvbn.measure("this is just a very long text without special characters or anything");
+        assertEquals(4, strength.getScore());
+        assertEquals(8.791855783542682E49, strength.getCrackTimeSeconds().getOnlineThrottling100perHour());
     }
 
 }
